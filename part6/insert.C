e55dedbaa9d0ba9417739cc8e20826f653e12d9f
buh-h <maxwellwang22@gmail.com>
2025-12-14 03:40:49 -0600

    insert.C implementation

--- part6/insert.C
+++ part6/insert.C
@@ -14,8 +14,48 @@ const Status QU_Insert(const string & relation,
 	const int attrCnt, 
 	const attrInfo attrList[])
 {
-// part 6
-return OK;
+	Status status;
+	AttrDesc attrDescArray[attrCnt];
+    int reclen = 0;
 
+    for (int i = 0; i < attrCnt; i++) {
+        status = attrCat->getInfo(relation, 
+                                  attrList[i].attrName, 
+                                  attrDescArray[i]);
+
+        if (status != OK) return status;
+        if (attrDescArray[i].attrType != attrList[i].attrType)
+            return ATTRTYPEMISMATCH;
+		
+		int currentEnd = attrDescArray[i].attrOffset + attrDescArray[i].attrLen;
+        if (currentEnd > reclen) reclen = currentEnd;
+    }
+
+    char outputData[reclen];
+    memset(outputData, 0, reclen);
+
+    for (int i = 0; i < attrCnt; i++) {
+        // Copy attrList data, uses attrOffset to place each value in relation order
+        char* dest = outputData + attrDescArray[i].attrOffset;
+        if (attrDescArray[i].attrType == INTEGER) {
+            int val = atoi((char*)attrList[i].attrValue);
+            memcpy(dest, &val, sizeof(int));
+        }
+        else if (attrDescArray[i].attrType == FLOAT) {
+            float val = atof((char*)attrList[i].attrValue);
+            memcpy(dest, &val, sizeof(float));
+        }
+        else memcpy(dest, attrList[i].attrValue, attrDescArray[i].attrLen);
+    }
+
+    Record outputRec;
+    outputRec.data = (void *) outputData;
+    outputRec.length = reclen;
+
+    InsertFileScan insertScan(relation, status);
+    if (status != OK) return status; 
+
+    RID outRID;
+    return insertScan.insertRecord(outputRec, outRID);
 }
 
